0f9ffef905cd3d170374d6061d87356824e6aaee
honzi <czechinthemail@gmail.com>
2017-06-10 01:14:14 -0700

    removed custom keydown and keyup, temporarily removed keybinds

--- js/click.js
+++ js/click.js
@@ -48,7 +48,6 @@ function repo_init(){
         'clicks-per-click-multiplied': 1,
         'clicks-per-second': 0,
         'clicks-per-second-multiplied': 0,
-        'keybind-click': 'C',
       },
       'title': 'Click.htm',
     });
@@ -67,14 +66,12 @@ function repo_init(){
             + core_storage_data['upgrade-' + id + '-cost'] + ',"'
             + 'clicks-' + upgrades[id]['type']
           + '") type=button value=' + upgrade + '>'
-          + '<input class=keybind id=keybind-' + id + ' maxlength=1>'
           + ' <span id=upgrade-' + id + '-cost>' + (upgrades[id]['base'] + core_storage_data['upgrade-' + id + '-cost']) +'</span><br>';
     }
 
     for(id in upgrades){
         var storage = {};
 
-        storage['keybind-' + id] = upgrades[id]['keybind'];
         storage['upgrade-' + id] = 0;
         storage['upgrade-' + id + '-cost'] = upgrades[id]['base'];
 
@@ -94,8 +91,6 @@ function repo_init(){
         }
     }
 
-    settings_toggle(false);
-
     document.getElementById('click-button').onclick = click_button;
     document.getElementById('reset-button').onclick = function(){
         if(core_storage_reset()){
@@ -105,60 +100,11 @@ function repo_init(){
             }
         }
     }
-    document.getElementById('settings-button').onclick = function(){
-        settings_toggle();
-    };
 
     window.setTimeout(
       second,
       1000
     );
-
-    window.onkeydown = function(e){
-        var key = e.keyCode || e.which;
-
-        // +: show settings.
-        if(key === 187){
-            settings_toggle(true);
-            return;
-
-        // -: hide settings.
-        }else if(key === 189){
-            settings_toggle(false);
-            return;
-        }
-
-        key = String.fromCharCode(key);
-
-        if(key === core_storage_data['keybind-click']){
-            if(keyclick_ready > 0){
-                keyclick_ready = 0;
-                click_button();
-            }
-
-            return;
-        }
-
-        for(var id in upgrades){
-            if(key != core_storage_data['keybind-' + id]){
-                continue;
-            }
-
-            purchase(
-              id,
-              core_storage_data['upgrade-' + id + '-cost'],
-              'clicks-' + upgrades[id]['type']
-            );
-        }
-    };
-
-    window.onkeyup = function(e){
-        var key = String.fromCharCode(e.keyCode || e.which);
-
-        if(key === core_storage_data['keybind-click']){
-            keyclick_ready = 1;
-        }
-    };
 
     core_storage_update();
 }
@@ -178,70 +124,41 @@ function second(){
     );
 }
 
-function settings_toggle(state){
-    var settings_button = document.getElementById('settings-button');
-
-    state = state == void 0
-      ? settings_button.value === '+'
-      : state;
-    var display = 'inline';
-
-    if(state){
-        settings_button.value = '-';
-
-    }else{
-        display = 'none';
-        settings_button.value = '+';
-    }
-
-    for(var id in upgrades){
-        document.getElementById('keybind-' + id).style.display = display;
-    }
-    document.getElementById('keybind-click').style.display = display;
-    document.getElementById('settings-div').style.display = display;
-}
-
 var keyclick_ready = 1;
 var upgrades = {
   'manual': {
     'base': 2,
     'bonus': 1,
-    'keybind': '1',
     'multiplier': 2,
     'type': 'per-click',
   },
   'script': {
     'base': 20,
     'bonus': 1,
-    'keybind': '2',
     'multiplier': 4,
     'type': 'per-second',
   },
   'employee': {
     'base': 50,
     'bonus': 2,
-    'keybind': '3',
     'multiplier': 8,
     'type': 'per-second',
   },
   'server': {
     'base': 100,
     'bonus': 3,
-    'keybind': '4',
     'multiplier': 16,
     'type': 'per-second',
   },
   'cluster': {
     'base': 200,
     'bonus': 4,
-    'keybind': '5',
     'multiplier': 32,
     'type': 'per-second',
   },
   'investor': {
     'base': 100,
     'bonus': 1,
-    'keybind': '6',
     'multiplier': 64,
     'type': 'multiplier',
   },
