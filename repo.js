c5daf031631ee2a709e2ee8e66ff7ca7cec3a3de
Jan Hořava <czechinthemail@gmail.com>
2023-07-18 12:48:24 -0700

    combined js into repo.js

--- repo.js
+++ repo.js
@@ -1,5 +1,68 @@
 'use strict';
 
+function click_button(){
+    core_storage_data['clicks'] = core_round({
+      'decimals': 2,
+      'number': core_storage_data['clicks'] + core_storage_data['clicks-per-click'] * core_storage_data['multiplier-per-click'],
+    });
+
+    core_storage_update({
+      'keys': [
+        'clicks',
+      ],
+    });
+
+    core_ui_update({
+      'ids': {
+        'ui-clicks': core_number_format({
+          'decimals-min': 2,
+          'number': core_storage_data['clicks'],
+        }),
+      },
+    });
+
+    document.getElementById('click-button').blur();
+}
+
+function purchase(upgrade, cost, target, free){
+    free = free || false;
+
+    if(!free
+      && core_storage_data['clicks'] < core_storage_data['upgrade-' + upgrade + '-cost']){
+        return;
+    }
+
+    if(!free){
+        core_storage_data['clicks'] = core_round({
+          'decimals': 2,
+          'number': core_storage_data['clicks'] - core_storage_data['upgrade-' + upgrade + '-cost'],
+        });
+        core_ui_update({
+          'ids': {
+            'ui-clicks': core_number_format({
+              'decimals-min': 2,
+              'number': core_storage_data['clicks'],
+            }),
+          },
+        });
+    }
+
+    core_storage_data['upgrade-' + upgrade] += 1;
+    core_storage_data['upgrade-' + upgrade + '-cost'] *= upgrades[upgrade]['multiplier'];
+    document.getElementById('ui-upgrade-' + upgrade + '-cost').textContent = core_number_format({
+      'decimals-min': 0,
+      'number': core_storage_data['upgrade-' + upgrade + '-cost'],
+    });
+
+    core_storage_data[target] = core_round({
+      'decimals': 2,
+      'number': core_storage_data[target] + upgrades[upgrade]['bonus'],
+    });
+
+    update_multiplied();
+    core_storage_update();
+}
+
 function repo_init(){
     core_repo_init({
       'events': {
@@ -124,5 +187,45 @@ function repo_init(){
           'number': core_storage_data['clicks'],
         }),
       },
+    });
+}
+
+function second(){
+    core_storage_data['clicks'] = core_round({
+      'decimals': 2,
+      'number': core_storage_data['clicks'] + core_storage_data['clicks-per-second'] * core_storage_data['multiplier-per-second'],
+    });
+
+    core_storage_update({
+      'keys': [
+        'clicks',
+      ],
+    });
+    core_storage_save({
+      'keys': [
+        'clicks',
+      ],
+    });
+
+    const formatted = core_number_format({
+      'decimals-min': 2,
+      'number': core_storage_data['clicks'],
+    });
+    core_ui_update({
+      'ids': {
+        'ui-clicks': formatted,
+      },
+    });
+    document.title = formatted + ' - ' + core_repo_title;
+}
+
+function update_multiplied(){
+    core_storage_data['clicks-per-click-multiplied'] = core_round({
+      'decimals': 2,
+      'number': core_storage_data['clicks-per-click'] * core_storage_data['multiplier-per-click'],
+    });
+    core_storage_data['clicks-per-second-multiplied'] = core_round({
+      'decimals': 2,
+      'number': core_storage_data['clicks-per-second'] * core_storage_data['multiplier-per-second'],
     });
 }
